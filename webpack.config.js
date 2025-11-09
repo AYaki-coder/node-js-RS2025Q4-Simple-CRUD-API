34a793ff675b3127bec0b70e47c0cd68b56aea8d
ayaki-coder <sj.a.yakimovich@gmail.com>
2025-11-09 22:33:55 +0300

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+
+  externals: [nodeExternals()],
+};
